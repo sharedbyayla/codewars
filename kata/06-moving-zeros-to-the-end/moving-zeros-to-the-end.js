8092a5c3b8a685b91ab89f8b6fa08de465629eac
Ayla Ex <aylaex@protonmail.com>
2021-05-29 07:37:56 -0400

    refactor

--- kata/06-moving-zeros-to-the-end/moving-zeros-to-the-end.js
+++ kata/06-moving-zeros-to-the-end/moving-zeros-to-the-end.js
@@ -1,11 +1,8 @@
 // https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript/5e7fcf45449b0d002059c1f1
 
 const moveZeros = (arr) => {
-	// filter for and store zeros
 	const zeros = arr.filter((val) => val === 0)
-	// filter out zeros
 	arr = arr.filter((val) => val !== 0)
-	// push each zero to the end of the array
 	zeros.map((zero) => arr.push(zero))
 
 	return arr
