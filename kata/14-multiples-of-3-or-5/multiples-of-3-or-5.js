3c68e43b93871bd57d364a2b160c1c9bc2f9f212
Ayla Ex <aylaex@protonmail.com>
2021-05-29 12:02:17 -0400

    refactor

--- kata/14-multiples-of-3-or-5/multiples-of-3-or-5.js
+++ kata/14-multiples-of-3-or-5/multiples-of-3-or-5.js
@@ -1,7 +1,14 @@
 // https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
 const solution = (number) => {
-	if (number < 0) return 0
+	let sum = 0
+	for (let i = 1; i < number; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			sum += i
+		}
+	}
+
+	return sum
 }
 
-solution
+solution(10) // -> 23 (3 + 5 + 6 + 9)
