8d8691fd2727510d125d3954fcb295173ca32175
Ayla Ex <aylaex@protonmail.com>
2021-05-29 12:01:41 -0400

    refactor

---
+++ kata/15-remove-anchor-from-url/remove-anchor-from-url.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/javascript
+
+const removeUrlAnchor = (str) => str.split('#')[0]
+
+removeUrlAnchor('www.codewars.com#about') // -> 'www.codewars.com'
+removeUrlAnchor('www.codewars.com?page=1') // -> 'www.codewars.com?page=1'
