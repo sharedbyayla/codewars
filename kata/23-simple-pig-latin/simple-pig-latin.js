58ce0b6eec3da2d5e5c5b1de66abf6b147c33bbd
Ayla Ex <aylaex@protonmail.com>
2021-06-02 19:24:33 -0400

    completes challenge

---
+++ kata/23-simple-pig-latin/simple-pig-latin.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+
+const pigIt = (str) => {
+	return str
+		.split(' ')
+		.map((word) =>
+			!word.match(/[a-z]/gi)
+				? word
+				: (word = word.substring(1) + word.substring(0, 1) + 'ay')
+		)
+		.join(' ')
+}
+pigIt('Pig latin is cool') // -> 'igPay atinlay siay oolcay'
+pigIt('Hello world !') // -> 'elloHay orldway !'
+pigIt('Pig latin is cool') // -> 'igPay atinlay siay oolcay'
+pigIt('This is my string') // -> 'hisTay siay ymay tringsay'
