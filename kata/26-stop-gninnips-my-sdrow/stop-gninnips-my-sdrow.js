e8f8b0344f2c8708b9c418c14c056d3e38037659
Ayla Ex <aylaex@protonmail.com>
2021-06-05 16:11:09 -0400

    completes challenge

---
+++ kata/26-stop-gninnips-my-sdrow/stop-gninnips-my-sdrow.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+
+const spinWords = (string) => {
+  return string.split(' ').map(word => word.length < 5 ? word : word.split('').reverse().join('') ).join(' ')
+}
+
+spinWords("Welcome") // -> "emocleW"
+spinWords("Hey fellow warriors") // -> "Hey wollef sroirraw"
+spinWords("This is a test") // -> "This is a test"
+spinWords("This is another test") // -> "This is rehtona test"
+spinWords("You are almost to the last test") // -> "You are tsomla to the last test"
+spinWords("Just kidding there is still one more") // -> "Just gniddik ereht is llits one more"
+spinWords("Seriously this is the last one") // -> "ylsuoireS this is the last one"
