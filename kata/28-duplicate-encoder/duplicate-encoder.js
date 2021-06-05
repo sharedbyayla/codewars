6bd845e49a27077c05facbca84cb743ffb6a795a
Ayla Ex <aylaex@protonmail.com>
2021-06-05 16:37:53 -0400

    completes challenge

---
+++ kata/28-duplicate-encoder/duplicate-encoder.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+
+const duplicateEncode = (word) => {
+  let letters = word.toLowerCase().split('')
+
+  return letters.map(val => {
+    let filtered = letters.filter(letter => letter === val)
+    return filtered.length > 1 ? ')' : '('
+  }).join('')
+}
+
+duplicateEncode("din") // -> "((("
+duplicateEncode("recede") // -> "()()()"
+duplicateEncode("Success") // -> ")())())" should ignore case
+duplicateEncode("(( @") // -> "))(("
