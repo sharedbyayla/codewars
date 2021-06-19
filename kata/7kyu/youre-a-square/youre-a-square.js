2ff5994980f188f4331abd7c8f8312d81f4d02bc
Ayla Ex <aylaex@protonmail.com>
2021-06-19 16:27:40 -0400

    completes challenge

---
+++ kata/7kyu/youre-a-square/youre-a-square.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+
+const isSquare = (n) => {
+  for (let i = 0; i <= n; i++) {
+    if (i * i === n) return true
+  }
+
+  return false
+}
+
+isSquare(-1) // -> false
+isSquare(0) // -> true
+isSquare(3) // -> false
+isSquare(4) // -> true
+isSquare(25) // -> true
+isSquare(26) // -> false
