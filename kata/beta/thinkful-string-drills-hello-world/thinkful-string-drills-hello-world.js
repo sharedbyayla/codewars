669358192280196e31636d9b638dd477067e6c5f
Ayla Ex <aylaex@protonmail.com>
2021-07-13 11:39:42 -0400

    completes challenge

---
+++ kata/beta/thinkful-string-drills-hello-world/thinkful-string-drills-hello-world.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5859d3325a8a0747180000c6
+
+const hello = (name) => {
+  return `Hello, ${name}`
+}
+
+hello('World') // -> 'Hello, World')
+hello('Grae') // -> 'Hello, Grae')
+hello('Dan') // -> 'Hello, Dan')
+hello('Alex') // -> 'Hello, Alex')
+hello('Bethany') // -> 'Hello, Bethany')
+hello('Darrell') // -> 'Hello, Darrell')
