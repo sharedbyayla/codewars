b375e8e96852ba0a0d0d17124262dbd709c88790
Ayla Ex <aylaex@protonmail.com>
2021-08-19 09:49:14 -0400

    completes challenge

---
+++ kata/8kyu/jennys-secret-message/jennys-secret-message.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390
+
+function greet(name) {
+  if (name === 'Johnny') return 'Hello, my love!'
+  return 'Hello, ' + name + '!'
+}
+
+greet('Jim') // -> "Hello, Jim!"
+greet('Jane') // -> "Hello, Jane!"
+greet('Simon') // -> "Hello, Simon!"
+greet('Johnny') // -> "Hello, my love!"
