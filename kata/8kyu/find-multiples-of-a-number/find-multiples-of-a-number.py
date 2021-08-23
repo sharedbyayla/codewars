d7e631ed0316b4989ee9b0e3085c333ca8dd403e
Ayla Ex <aylaex@protonmail.com>
2021-08-23 19:57:38 -0400

    refactor

--- kata/8kyu/find-multiples-of-a-number/find-multiples-of-a-number.py
+++ kata/8kyu/find-multiples-of-a-number/find-multiples-of-a-number.py
@@ -1,11 +1,7 @@
 # https://www.codewars.com/kata/58ca658cc0d6401f2700045f
 
 def find_multiples(integer, limit):
-	multiples = []
-	for num in range(integer, limit + 1, integer):
-		multiples.append(num)
-
-	print(multiples)
+	print(list(range(integer, limit + 1, integer)))
 
 
 find_multiples(5, 25) # -> [5, 10, 15, 20, 25]
