f078f19ea0bfbcd8a39a890d2e874b9e881e2ec3
Ayla Ex <aylaex@protonmail.com>
2021-08-23 19:02:23 -0400

    completes challenge

---
+++ kata/8kyu/opposite-number/opposite-number.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+def opposite(number):
+  return -number
+
+opposite(1) # -> -1
+opposite(25.6) # -> -25.6
+opposite(0) # -> 0
+opposite(1425.2222) # -> -1425.2222
+opposite(-3.1458) # -> 3.1458
+opposite(-95858588225)# -> 95858588225
